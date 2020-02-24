f6553bd1ea100dc7b3ee59d7319bc60f2eda31fb
David Webb <dw9694@gmail.com>
2020-02-24 20:04:23 +0300

    refactor handlerVideo, add logging

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/dimonchik0036/vk-api"
 	"log"
 	"net/http"
@@ -12,41 +11,31 @@ import (
 
 var client, _ = vkapi.NewClientFromToken(os.Getenv("VK_TOKEN"))
 
-func VideoOptions(r *http.Request) url.Values {
+func main() {
+	http.HandleFunc("/video", handlerVideo)
+	log.Printf("[INFO] start server on port %d\n", 7531)
+	log.Fatal(http.ListenAndServe(":7531", nil))
+}
+
+func handlerVideo(w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query()
 	v := url.Values{}
-
-	v.Set("q", q.Get("query"))
+	v.Set("q", q.Get("q"))
 	v.Set("filters", q.Get("filters"))
 	v.Set("longer", q.Get("longer"))
 	v.Set("count", q.Get("count"))
-	return v
-}
 
-func GetVideo(r *http.Request) []byte {
-	v := VideoOptions(r)
 	res, err := client.Do(vkapi.NewRequest("video.search", "", v))
 	if err != nil {
-		panic(err)
+		panic(err.Error())
 	}
 
-	items, _ := json.Marshal(res)
-	return items
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	items := GetVideo(r)
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-	_, err := w.Write(items)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func main() {
-	http.HandleFunc("/", handler)
-	fmt.Printf("[INFO] start server on port %d\n", 7531)
-	log.Fatal(http.ListenAndServe(":7531", nil))
+	items, _ := json.Marshal(res)
+	values, _ := json.Marshal(v)
+	w.Write(items)
+	log.Printf("[REQUEST] %s\n", values)
+	log.Printf("[RESPONSE] %s\n", items)
 }
