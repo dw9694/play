93ec194ca1024abc58c1e7bd13cb4688717feb8a
David Webb <dw9694@gmail.com>
2020-03-06 12:15:11 +0300

    refactor code

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"github.com/dimonchik0036/vk-api"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 )
 
@@ -13,28 +12,19 @@ var client, _ = vkapi.NewClientFromToken(os.Getenv("VK_TOKEN"))
 
 func main() {
 	http.HandleFunc("/video", handlerVideo)
+
 	log.Printf("[INFO] start server on port %d\n", 7531)
 	log.Fatal(http.ListenAndServe(":7531", nil))
 }
 
 func handlerVideo(w http.ResponseWriter, r *http.Request) {
-	q := r.URL.Query()
-	v := url.Values{}
-	v.Set("q", q.Get("q"))
-	v.Set("filters", q.Get("filters"))
-	v.Set("longer", q.Get("longer"))
-	v.Set("count", q.Get("count"))
-
-	res, err := client.Do(vkapi.NewRequest("video.search", "", v))
+	res, err := client.Do(vkapi.NewRequest("video.search", "", r.URL.Query()))
 	if err != nil {
 		panic(err.Error())
 	}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	items, _ := json.Marshal(res)
-	if _, err := w.Write(items); err != nil {
-		panic(err.Error())
-	}
+	w.Write(items)
 }
